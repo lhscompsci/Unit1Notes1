225cd15820658821b703f091f0229752769c3b1a
Sam Black <sam.black@lubbockisd.org>
2022-09-07 09:39:53 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //variables
+
+        // 4 integer-based types (whole numbers)
+
+        byte value = 28;                // values range from -128 to 127        8-bit
+        short anotherValue = -15937;    // values range from -32768 to 32767    16-bit
+        int mostPopularValue = 42;      // values range from -2147483648 to 2147483647
+                                        //                                      32-bit
+        long extremelyBigNum=-831L;      // values range from -9quintillion to 9quintillion
+                                        //                                      64-bit
+
+
+        //2 real number types (can have decimals)
+
+        float realValue = 3.6f;          // values range from -big to big with ~7 significant digits
+                                            //              32-bit
+        double popularRealValue = -324.86245;   //      " " " " with ~15 significant digits
+                                            //              64-bit
+
+        //boolean (true/false)
+
+        
+
+        //character data -- a SINGLE character
+
+
+    }
+}
